866fc22fa1a3a3c24e710ae1d574891edc0dc177
tommytracey <tommytracey@gmail.com>
2017-11-15 20:47:43 -0600

    fixed H_jacobian reference

--- p1-extended-kalman-filter/src/FusionEKF.cpp
+++ p1-extended-kalman-filter/src/FusionEKF.cpp
@@ -151,13 +151,13 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
 
     // Radar updates
-    H_jacobian = tools.CalculateJacobian(ekf_.x_);
-    ekf_.H_ = H_jacobian;
+    H_j = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = H_j;
     ekf_.R_ = R_radar_;
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
-    
+
     // Laser updates
     ekf_.H_ = H_laser_;
     ekf_.R_ = R_laser_;
