d5cc049e922a8b833d76b8da4dd13d3f56818552
tommytracey <tommytracey@gmail.com>
2017-11-15 20:17:32 -0600

    added update logic

--- p1-extended-kalman-filter/src/FusionEKF.cpp
+++ p1-extended-kalman-filter/src/FusionEKF.cpp
@@ -149,9 +149,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
    */
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+
     // Radar updates
+    H_jacobian = tools.CalculateJacobian(ekf_.x_);
+    ekf_.H_ = H_jacobian;
+    ekf_.R_ = R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+
   } else {
+    
     // Laser updates
+    ekf_.H_ = H_laser_;
+    ekf_.R_ = R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
