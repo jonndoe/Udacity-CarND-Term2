4b94b16495021b22761191df6150a48ffb641ca0
tommytracey <tommytracey@gmail.com>
2017-12-09 14:43:21 +0200

    added augmentation logic

--- p2-unscented-kalman-filter/src/ukf.cpp
+++ p2-unscented-kalman-filter/src/ukf.cpp
@@ -62,7 +62,40 @@ UKF::UKF() {
 
   // time when the state is true, in us
   time_us_ = 0.0;
-  
+
+  // state dimension
+  n_x = 5;
+
+  // augmented dimension
+  n_aug = 7;
+
+  // spreading parameter
+  lambda = 3 - n_aug;
+
+  //set example state
+  VectorXd x = VectorXd(n_x);
+  x <<   5.7441,
+         1.3800,
+         2.2049,
+         0.5015,
+         0.3528;
+
+  //create example covariance matrix
+  MatrixXd P = MatrixXd(n_x, n_x);
+  P <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+          -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+           0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+          -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+          -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+
+  //create augmented mean vector
+  VectorXd x_aug = VectorXd(7);
+
+  //create augmented state covariance
+  MatrixXd P_aug = MatrixXd(7, 7);
+
+  //create sigma point matrix
+  MatrixXd Xsig_aug = MatrixXd(n_aug, 2 * n_aug + 1);  
 
 }
 
