073dc4bbaa9db43c817c76851e22cd5f32947651
tommytracey <tommytracey@gmail.com>
2018-01-01 19:57:08 +0200

    abandoned the initial approach

--- p4-PID-control/src/PID.h
+++ p4-PID-control/src/PID.h
@@ -9,25 +9,13 @@ public:
   double p_error;
   double i_error;
   double d_error;
-  double total_error_;
-
-  // cross track errors
-  double cte_;
-  double cte_prev_;
-  double cte_mem_;
 
   /*
   * Coefficients
-  */
-  double Kp_, Ki_, Kd_;
-  double Kp0_, Ki0_, Kd0_;
-  double alpha_p_, alpha_i_, alpha_d_;
-
-  /*
-  * Other parameters
-  */
-  double v_;
-  double mem_frac_;
+  */ 
+  double Kp;
+  double Ki;
+  double Kd;
 
   /*
   * Constructor
@@ -42,7 +30,7 @@ public:
   /*
   * Initialize PID.
   */
-  void Init(double Kp0, double Ki0, double Kd0, double alpha_p, double alpha_i,double alpha_d, double v, double mem_frac);
+  void Init(double Kp, double Ki, double Kd);
 
   /*
   * Update the PID error variables given cross track error.
